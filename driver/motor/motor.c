8d0809d371527719a4f47cb6766e3351ea8663be
engeechung <2201257@sit.singaporetech.edu.sg>
2023-11-01 21:48:32 +0800

    motor driver
    v1

---
+++ driver/motor/motor.c
@@ -0,0 +1,94 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+#include "hardware/pwm.h"
+#include "hardware/gpio.h"
+#include "motor.h"
+
+uint slice_num_left;
+uint slice_num_right;
+
+void initMotor(void *params) {
+    gpio_set_function(LEFT_WHEEL, GPIO_FUNC_PWM);
+    gpio_set_function(RIGHT_WHEEL, GPIO_FUNC_PWM);
+
+    slice_num_left = pwm_gpio_to_slice_num(LEFT_WHEEL);
+    slice_num_right = pwm_gpio_to_slice_num(RIGHT_WHEEL);
+
+    gpio_init(RIGHT_WHEEL_FORWARD);
+    gpio_init(RIGHT_WHEEL_BACKWARD);
+    gpio_init(LEFT_WHEEL_FORWARD);
+    gpio_init(LEFT_WHEEL_BACKWARD);
+
+    gpio_set_dir(RIGHT_WHEEL_FORWARD, GPIO_OUT);
+    gpio_set_dir(RIGHT_WHEEL_BACKWARD, GPIO_OUT);
+    gpio_set_dir(LEFT_WHEEL_FORWARD, GPIO_OUT);
+    gpio_set_dir(LEFT_WHEEL_BACKWARD, GPIO_OUT);
+}
+
+void set_left_speed(float speed){
+    pwm_set_clkdiv(slice_num_left, CLK_DIV);
+    pwm_set_wrap(slice_num_left, WRAP);
+    pwm_set_chan_level(slice_num_left, PWM_CHAN_A, WRAP * speed);
+    pwm_set_enabled(slice_num_left, true);
+}
+
+void set_right_speed(float speed){
+    pwm_set_clkdiv(slice_num_right, CLK_DIV);
+    pwm_set_wrap(slice_num_right, WRAP);
+    pwm_set_chan_level(slice_num_right, PWM_CHAN_B, WRAP * speed);
+    pwm_set_enabled(slice_num_right, true);
+}
+
+void stop(void *params) {
+    gpio_put(RIGHT_WHEEL_FORWARD, 0);
+    gpio_put(RIGHT_WHEEL_BACKWARD, 0);
+    gpio_put(LEFT_WHEEL_FORWARD, 0);
+    gpio_put(LEFT_WHEEL_BACKWARD, 0);
+}
+
+void move_forward(void *params) {
+    gpio_put(RIGHT_WHEEL_FORWARD, 1);
+    gpio_put(RIGHT_WHEEL_BACKWARD, 0);
+    gpio_put(LEFT_WHEEL_FORWARD, 1);
+    gpio_put(LEFT_WHEEL_BACKWARD, 0);
+}
+
+void move_backward(void *params) {
+    gpio_put(RIGHT_WHEEL_FORWARD, 0);
+    gpio_put(RIGHT_WHEEL_BACKWARD, 1);
+    gpio_put(LEFT_WHEEL_FORWARD, 0);
+    gpio_put(LEFT_WHEEL_BACKWARD, 1);
+}
+
+void turn_left(void *params) {
+    gpio_put(RIGHT_WHEEL_FORWARD, 1);
+    gpio_put(RIGHT_WHEEL_BACKWARD, 0);
+    gpio_put(LEFT_WHEEL_FORWARD, 0);
+    gpio_put(LEFT_WHEEL_BACKWARD, 1);
+}
+
+void turn_right(void *params) {
+    gpio_put(RIGHT_WHEEL_FORWARD, 0);
+    gpio_put(RIGHT_WHEEL_BACKWARD, 1);
+    gpio_put(LEFT_WHEEL_FORWARD, 1);
+    gpio_put(LEFT_WHEEL_BACKWARD, 0);
+}
+
+int main() {
+    initMotor();
+    set_left_speed(0.8);
+    set_right_speed(0.8);
+
+    move_forward();
+    sleep(2);
+    stop();
+    move_backward();
+    sleep(2);
+    stop();
+    turn_left();
+    sleep(2);
+    stop();
+    turn_right();
+    sleep(2);
+    stop();
+}
